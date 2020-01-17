1ac9454f3208a140c658d08d83587c0d84ad47f3
Tomasz Zalejski <210128@wp.pl>
2020-01-17 15:28:56 +0100

    zadanie1

--- kodilla-intro/src/main/java/FirstClass.java
+++ kodilla-intro/src/main/java/FirstClass.java
@@ -1,18 +1,21 @@
 public class FirstClass {
     public static void main(String[] args) {
-        Notebook notebook = new Notebook(600, 1000, 2018);
+        Notebook notebook = new Notebook(600, 1001, 2018);
         System.out.println(notebook.weight + notebook.price);
         Notebook heavyNotebook = new Notebook(2000, 500, 2015);
         System.out.println(heavyNotebook.weight);
         System.out.println(heavyNotebook.price);
-        Notebook odlNotebook = new Notebook(1200, 800, 2010);
-        System.out.println(odlNotebook.weight);
-        System.out.println(odlNotebook.price);
+        Notebook oldNotebook = new Notebook(1200, 800, 2010);
+        System.out.println(oldNotebook.weight);
+        System.out.println(oldNotebook.price);
         notebook.checkPrice();
         heavyNotebook.checkPrice();
-        odlNotebook.checkPrice();
+        oldNotebook.checkPrice();
         notebook.checkWeight();
         heavyNotebook.checkWeight();
-        odlNotebook.checkWeight();
+        oldNotebook.checkWeight();
+        notebook.checkYear();
+        heavyNotebook.checkYear();
+        oldNotebook.checkYear();
     }
 }
--- kodilla-intro/src/main/java/Notebook.java
+++ kodilla-intro/src/main/java/Notebook.java
@@ -20,11 +20,20 @@ public class Notebook {
     public void checkWeight() {
         if (this.weight < 1000) {
             System.out.println("This notebook is lightweight");
-        } else if (this.weight> 1000 && this.weight < 2000) {
+        } else if (this.weight < 2000) {
             System.out.println("This notebook is not to heavy");
         } else {
             System.out.println("This notebook is very heavy");
         }
 
+    }
+    public void checkYear() {
+        if (this.year < 2008 && this.price < 1000) {
+            System.out.println("The device is old and cheap");
+        } else if (this.year < 2016 && this.price < 2000) {
+            System.out.println("The device is not very old and inexpensive");
+        } else {
+        System.out.println("The device is quite new and expensive");
+        }
     }
 }
