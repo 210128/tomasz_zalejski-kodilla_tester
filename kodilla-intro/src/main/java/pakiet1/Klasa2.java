2db8d4e399aff172c6a6453442dde62c6aa203d7
Tomasz Zalejski <210128@wp.pl>
2020-01-18 11:37:52 +0100

    Nowa klasa2. nowy obiekt

--- kodilla-intro/src/main/java/pakiet1/Klasa2.java
+++ kodilla-intro/src/main/java/pakiet1/Klasa2.java
@@ -6,5 +6,11 @@ public class Klasa2 {
     public void cosTam() {
         Notebook notebook = new Notebook(0,0,0);
         notebook.price = 200;
+        System.out.println(notebook.price);
+    }
+
+    public static void main(String[] args) {
+       Klasa2 k2 = new Klasa2();
+       k2.cosTam();
     }
 }
