5335603705c7001663efc194a694bbc0f2d3b8bd
Tomasz Zalejski <210128@wp.pl>
2020-01-28 23:15:47 +0100

    homework Job fixed

--- kodilla-basic-tests/src/main/java/com/kodilla/abstracts/homework/Job.java
+++ kodilla-basic-tests/src/main/java/com/kodilla/abstracts/homework/Job.java
@@ -1,13 +1,13 @@
 package com.kodilla.abstracts.homework;
 
-public abstract class Job  {
+public abstract class Job {
 
     private int salary;
     private String responsibilities;
 
-    public Job(int salary, String responsibilities){
-        this.salary=salary;
-        this.responsibilities=responsibilities;
+    public Job(int salary, String responsibilities) {
+        this.salary = salary;
+        this.responsibilities = responsibilities;
     }
 
     public int getSalary() {
@@ -17,7 +17,8 @@ public abstract class Job  {
     public String getResponsibilities() {
         return responsibilities;
     }
-    public  void displayResponsibilities(){
+
+    public void displayResponsibilities() {
         System.out.println(getResponsibilities());
     }
 
