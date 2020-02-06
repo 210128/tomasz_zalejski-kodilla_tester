fb9c47ec3a727d890dd8a31dcecf815b07cdfc24
Tomasz Zalejski <210128@wp.pl>
2020-02-06 21:06:18 +0100

    exercise Countries

--- kodilla-collections/src/main/java/com/kodilla/collections/lists/Application.java
+++ kodilla-collections/src/main/java/com/kodilla/collections/lists/Application.java
@@ -6,7 +6,7 @@ public class Application {
     public static void main(String[] args) {
         List<Country> countries = List.of(new Poland("Polish"), new France("French"), new Germany("German"));
 
-        for (Country country: countries) {
+        for (Country country : countries) {
             System.out.print(country.getClass().getSimpleName() + "-");
             System.out.println(country.getLanguage());   // coś jeszcze miało być zrobione?
         }
