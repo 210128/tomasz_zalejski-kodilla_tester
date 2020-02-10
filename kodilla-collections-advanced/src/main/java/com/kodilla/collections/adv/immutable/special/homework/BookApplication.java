52011fbda1523caca9d31d346ea27267c3d94d17
KDRZAZGA <krzysztof.drzazga@op.pl>
2020-02-10 19:48:08 +0100

    exercise 5.4//Review

--- kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/immutable/special/homework/BookApplication.java
+++ kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/immutable/special/homework/BookApplication.java
@@ -3,7 +3,7 @@ package com.kodilla.collections.adv.immutable.special.homework;
 public class BookApplication {
     public static void main(String[] args) {
         BookManager book = new BookManager();
-        book.createBook("Inferno", "Dan Brown");
+        book.createBook("Inferno", "Dan Brown");/*TODO: Review :assign this to a varaible, then compare with others*/
         book.createBook("Witcher", "Andrzej Sapkowski");
         book.createBook("Tengu", "Graham Masterton");
         book.createBook("Inferno", "Dan Brown");
@@ -11,6 +11,7 @@ public class BookApplication {
 
 book.displayBooks();
 
+/*TODO Review - please remove blank lines and format*/
 
 
     }
--- kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/immutable/special/homework/BookManager.java
+++ kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/immutable/special/homework/BookManager.java
@@ -8,7 +8,13 @@ public class BookManager {
     Set<Book> books = new HashSet<>();
 
     public Book createBook(String title, String author) {
-        books.add(new Book(title, author));
+
+
+        books.add(new Book(title, author));  /*TODO Review - 2 objects are created in theis method, please:
+        1. create 1 object Book
+        2. add it to the list
+        3. return it*/
+
         return new Book(title, author);
     }
 
